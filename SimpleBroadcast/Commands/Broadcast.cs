2608636c168bf88b2547b11c48bb3175622593f2
William Rhodes <frhodes364@gmail.com>
2024-02-16 22:02:24 -0500

    Add project files.

---
+++ SimpleBroadcast/Commands/Broadcast.cs
@@ -0,0 +1,27 @@
+﻿using OpenMod.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Color = System.Drawing.Color;
+
+namespace SimpleBroadcast.Commands
+{
+    [Command("broadcast")]
+    [CommandAlias("bc")]
+    [CommandDescription("Broadcast to all players on the sevrer")]
+    [CommandSyntax("<message>")]
+    public class Broadcast : Command
+    {
+        public Broadcast(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        protected override async Task OnExecuteAsync()
+        {
+            var message = await Context.Parameters.GetAsync<string>(0);
+
+            await PrintAsync(message, Color.Lime);
+        }
+    }
+}
